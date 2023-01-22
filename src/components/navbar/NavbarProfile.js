bd6c3e10789bd99f6fbdb3261089c1bc249178fb
Cupu-Boy <fajrir357@gmail.com>
2023-01-22 18:43:32 +0700

    update navbar

--- src/components/navbar/NavbarProfile.js
+++ src/components/navbar/NavbarProfile.js
@@ -10,8 +10,8 @@ import { getProfilePicture } from "../../redux/actions/profile";
 const NavbarProfile = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const token = useSelector(state => state.auth.token)
-  const profilePicture = useSelector(state => state.profile.picture)
+  const token = useSelector((state) => state.auth.token);
+  const profilePicture = useSelector((state) => state.profile.picture);
 
   const logout = () => {
     dispatch(logoutAction());
@@ -19,70 +19,65 @@ const NavbarProfile = () => {
   };
 
   useEffect(() => {
-    dispatch(getProfilePicture(token))
-  }, [token])
+    dispatch(getProfilePicture(token));
+  }, [token]);
 
   return (
     <>
-    <nav className="navbar flex bg-base-100 sm:hidden px-7 pt-5 py-5">
-        <div className="mr-7">
-          <div className="mt-3 text-4xl">☰</div>
+      <nav className="navbar flex bg-base-100 sm:hidden px-7 pt-5 py-5">
+        <div className="dropdown">
+          <label tabIndex={0} className="mt-3 text-4xl">
+            ☰
+          </label>
+          <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+            <li>
+              <button onClick={() => navigate("/profile")}>Profile</button>
+            </li>
+            <li>
+              <button  type="button" onClick={() => logout()}>Logout</button>
+            </li>
+          </ul>
         </div>
+
         <div className="flex-1">
           <img className="w-32" src={navIcon} alt="icon logo" />
         </div>
+      </nav>
 
-        <div className="group relative">
-          <img src={defaultUser} alt="profile" />
-
-          <div className="hidden group-hover:block absolute right-0 top-14 border-2 border-[#dedede] bg-[#FCFDFE] py-2 pl-4 pr-8">
+      <nav className="hidden sm:flex py-5 px-10 md:px-14 lg:px-28 items-center">
+        <div>
+          <img className="w-32" src={navIcon} alt="icon logo" />
+        </div>
+        <div className="flex grow ml-4">
+          <Link className="mx-10" to="/">
+            Home
+          </Link>
+          <Link className="mx-10" to="/list-movie">
+            List Movie
+          </Link>
+        </div>
+        <div className="flex items-center justify-center gap-10">
+          <div className="group flex hover:border-2 hover:border-[#dedede] hover:bg-[#fcfdfe] rounded-2xl">
+            <img className="pl-3 mr-3" src={searchIcon} alt="search" />
             <div>
-              <button onClick={() => navigate("/profile")}>Profile</button>
+              <input className="hidden group-hover:flex py-3 pr-2 text-xs rounded-2xl bg-[#fcfdfe] focus:outline-none" type="search" name="search" id="search" placeholder="Search Movie Name ..." />
             </div>
-            <div>
-              <button type="button" onClick={() => logout()}>
-                Logout
-              </button>
+          </div>
+          <div className="group relative">
+            <img src={profilePicture || defaultUser} alt="profile" className="w-14 h-14 rounded-full " />
+            <div className="hidden group-hover:block absolute right-0 border-2 border-[#dedede] bg-[#FCFDFE] py-2 pl-4 pr-8">
+              <div>
+                <button onClick={() => navigate("/profile")}>Profile</button>
+              </div>
+              <div>
+                <button type="button" onClick={() => logout()}>
+                  Logout
+                </button>
+              </div>
             </div>
           </div>
         </div>
       </nav>
-
-
-    <nav className="hidden sm:flex py-5 px-10 md:px-14 lg:px-28 items-center">
-      <div>
-        <img className="w-32" src={navIcon} alt="icon logo" />
-      </div>
-      <div className="flex grow ml-4">
-        <Link className="mx-10" to="/">
-          Home
-        </Link>
-        <Link className="mx-10" to="/list-movie">
-          List Movie
-        </Link>
-      </div>
-      <div className="flex items-center justify-center gap-10">
-        <div className="group flex hover:border-2 hover:border-[#dedede] hover:bg-[#fcfdfe] rounded-2xl">
-          <img className="pl-3 mr-3" src={searchIcon} alt="search" />
-          <div>
-            <input className="hidden group-hover:flex py-3 pr-2 text-xs rounded-2xl bg-[#fcfdfe] focus:outline-none" type="search" name="search" id="search" placeholder="Search Movie Name ..." />
-          </div>
-        </div>
-        <div className="group relative">
-          <img src={profilePicture || defaultUser} alt="profile" className='w-14 h-14 rounded-full '/>
-          <div className="hidden group-hover:block absolute right-0 border-2 border-[#dedede] bg-[#FCFDFE] py-2 pl-4 pr-8">
-            <div>
-              <button onClick={() => navigate("/profile")}>Profile</button>
-            </div>
-            <div>
-              <button type="button" onClick={() => logout()}>
-                Logout
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </nav>
     </>
   );
 };
